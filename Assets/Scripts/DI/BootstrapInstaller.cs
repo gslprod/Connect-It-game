ac63d02cb74613975b8552fe5875eef22c4272d0
gslprod <gslprod.dev@gmail.com>
2023-03-16 16:34:57 +0500

    Add project files.

---
+++ Assets/Scripts/DI/BootstrapInstaller.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+public class BootstrapInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DI/Scene installers/GameSceneInstaller.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Zenject;
+
+public class GameSceneInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+    }
+}
