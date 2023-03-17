f263f87899e0b6597fa3479432e650b64f60c974
gslprod <gslprod.dev@gmail.com>
2023-03-17 16:47:00 +0500

    Add first implementation of Tile logic

---
+++ Assets/Scripts/Gameplay/Model/Tiles/Tile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConnectIt.Model
+{
+    public class Tile
+    {
+        public Vector2 LocationInMap { get; }
+
+        private List<TileUser> _users;
+
+        public Tile(Vector2 locationInMap)
+        {
+            LocationInMap = locationInMap;
+        }
+
+        public bool TryAddUser(TileUser toAdd)
+        {
+            if (toAdd == null)
+                return false;
+
+            bool canBeAdded = _users.TrueForAll(user => user.CanBeOtherUserAdded(toAdd));
+            if (canBeAdded)
+                _users.Add(toAdd);
+
+            return canBeAdded;
+        }
+
+        public bool TryRemoveUser(TileUser toRemove)
+        {
+            if (!ContainsUser(toRemove))
+                return false;
+
+            bool canBeRemoved = toRemove.CanBeRemoved;
+            if (canBeRemoved)
+                _users.Remove(toRemove);
+
+            return canBeRemoved;
+        }
+
+        public bool ContainsUser(TileUser toCheck)
+            => _users.Contains(toCheck);
+    }
+}
---
+++ Assets/Scripts/Gameplay/Model/Tiles/TileUser.cs
@@ -0,0 +1,17 @@
+﻿namespace ConnectIt.Model
+{
+    public abstract class TileUser
+    {
+        public Tile Tile { get; }
+
+        public abstract bool CanBeRemoved { get; }
+
+        public TileUser(Tile tile)
+        {
+            Tile = tile;
+        }
+
+        public virtual bool CanBeOtherUserAdded(TileUser other)
+            => false;
+    }
+}
