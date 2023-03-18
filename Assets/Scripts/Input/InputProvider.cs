e984885f9e7bdc2082ebd63872f1e4ae9224bf50
gslprod <gslprod.dev@gmail.com>
2023-03-19 00:44:03 +0500

    Add Input folder and classes

---
+++ Assets/Scripts/Input/InputProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace ConnectIt.Input
+{
+    public class InputProvider
+    {
+        private MainInput _input;
+
+        public InputProvider()
+        {
+            _input = new MainInput();
+        }
+
+        public void Enable()
+        {
+            _input.Enable();
+        }
+
+        public void Disable()
+        {
+            _input.Disable();
+        }
+    }
+}
