1b78257aec912988f94df1f3e90a6a5304c08976
gslprod <gslprod.dev@gmail.com>
2023-03-21 23:48:09 +0500

    Add Camera classes

---
+++ Assets/Scripts/Camera/CameraRegistrator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace ConnectIt
+{
+    public class CameraRegistrator : MonoBehaviour
+    {
+        private Camera _camera;
+        private RenderCameraProvider _renderCameraProvider;
+
+        [Inject]
+        public void Constructor(RenderCameraProvider renderCameraProvider)
+        {
+            _renderCameraProvider = renderCameraProvider;
+        }
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+
+            _renderCameraProvider.RegisterCamera(_camera);
+        }
+
+        private void OnDestroy()
+        {
+            _renderCameraProvider.UnregisterCamera(_camera);
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/RenderCameraProvider.cs
@@ -0,0 +1,54 @@
+﻿using ConnectIt.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConnectIt
+{
+    public class RenderCameraProvider
+    {
+        public Camera Current {  get; private set; }
+
+        private List<Camera> _camerasList = new();
+
+        public void RegisterCamera(Camera camera)
+        {
+            _camerasList.Add(camera);
+            Assert.That(!_camerasList.Contains(camera));
+
+            _camerasList.Add(camera);
+
+            if (Current == null)
+                SetRenderCamera(camera);
+        }
+
+        public void UnregisterCamera(Camera camera)
+        {
+            Assert.IsNotNull(camera);
+            Assert.That(_camerasList.Remove(camera));
+
+            if (Current == camera)
+            {
+                Current = null;
+
+                if (_camerasList.Count != 0)
+                    SetRenderCamera(_camerasList[0]);
+            }
+        }
+
+        public void SetRenderCamera(Camera toSet)
+        {
+            Assert.IsNotNull(toSet);
+            Assert.That(_camerasList.Contains(toSet));
+
+            foreach (var camera in _camerasList)
+            {
+                if (camera != toSet)
+                    camera.enabled = false;
+                else
+                    camera.enabled = true;
+            }
+
+            Current = toSet;
+        }
+    }
+}
