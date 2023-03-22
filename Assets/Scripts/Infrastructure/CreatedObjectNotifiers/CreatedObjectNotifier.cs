9a028b6443d142515255e886144018c562e50539
gslprod <gslprod.dev@gmail.com>
2023-03-22 18:32:31 +0500

    Add CreatedObjectNotifiers

---
+++ Assets/Scripts/Infrastructure/CreatedObjectNotifiers/CreatedObjectNotifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConnectIt.Infrastructure.CreatedObjectNotifiers
+{
+    public class CreatedObjectNotifier<T> : ICreatedObjectNotifier<T>
+    {
+        public event Action<T> Created;
+
+        public void SendNotification(T createdObject)
+        {
+            Created?.Invoke(createdObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/CreatedObjectNotifiers/ICreatedObjectNotifier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConnectIt.Infrastructure.CreatedObjectNotifiers
+{
+    public interface ICreatedObjectNotifier<T>
+    {
+        public event Action<T> Created;
+
+        public void SendNotification(T createdObject);
+    }
+}
