90217b3f1d95b1fb5963c36314bc6bf08f5beb80
gslprod <gslprod.dev@gmail.com>
2023-03-30 18:47:51 +0500

    Start creating gameplay UI, import Fonts and Sprites for UI

---
+++ Assets/Scripts/Gameplay/GameStateObserver.cs
@@ -0,0 +1,18 @@
+﻿using ConnectIt.Gameplay.Model;
+using ConnectIt.Infrastructure.Registrators;
+
+namespace ConnectIt.Gameplay
+{
+    public class GameStateObserver
+    {
+        private readonly Tilemaps _tilemaps;
+        private readonly IRegistrator<Port> _portRegistrator;
+
+        public GameStateObserver(Tilemaps tilemaps,
+            IRegistrator<Port> portRegistrator)
+        {
+            _tilemaps = tilemaps;
+            _portRegistrator = portRegistrator;
+        }
+    }
+}
